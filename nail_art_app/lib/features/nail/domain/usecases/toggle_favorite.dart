33c9ca6c67581502a871858761c5e2d094f3ed9c
aysecerentekin <cerntkn33@gmail.com>
2026-05-15 22:04:11 +0300

    Update toggle_favorite.dart

--- nail_art_app/lib/features/nail/domain/usecases/toggle_favorite.dart
+++ nail_art_app/lib/features/nail/domain/usecases/toggle_favorite.dart
@@ -1,4 +1,5 @@
 // lib/features/nail/domain/usecases/toggle_favorite.dart
+//favori durumunu değiştirmek.
 
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
@@ -6,19 +7,20 @@ import '../../../../core/errors/failures.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../repositories/nail_repository.dart';
 
+//ürünü favoriye ekleme ve çıkarma işlemin yönetir.
 class ToggleFavorite implements UseCase<bool, ToggleFavoriteParams> {
   final NailRepository repository;
   ToggleFavorite(this.repository);
 
   @override
   Future<Either<Failure, bool>> call(ToggleFavoriteParams params) async {
-    return repository.toggleFavorite(params.id, params.isFavorite);
+    return repository.toggleFavorite(params.id, params.isFavorite); //favori durumunu repository üzerinden güncelleme.
   }
 }
 
 class ToggleFavoriteParams extends Equatable {
   final String id;
-  final bool isFavorite;
+  final bool isFavorite; //yeni favori durumu
   const ToggleFavoriteParams({required this.id, required this.isFavorite});
 
   @override
