d994c242c73f85d9fe7b3a57b0aa724fb2287909
aysecerentekin <cerntkn33@gmail.com>
2026-05-15 21:15:02 +0300

    Update exceptions.dart

--- nail_art_app/lib/core/errors/exceptions.dart
+++ nail_art_app/lib/core/errors/exceptions.dart
@@ -1,6 +1,6 @@
 // lib/core/errors/exceptions.dart
 
-class ServerException implements Exception {
+class ServerException implements Exception { //sunucu ve backend hatalarında çalışır.
   final String message;
   const ServerException({this.message = 'Sunucu hatası'});
 }
@@ -10,7 +10,7 @@ class CacheException implements Exception {
   const CacheException({this.message = 'Cache hatası'});
 }
 
-class NetworkException implements Exception {
+class NetworkException implements Exception { //bağlantı sorunlarında çalışır.
   final String message;
   const NetworkException({this.message = 'Ağ hatası'});
 }
